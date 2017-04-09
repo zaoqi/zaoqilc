b48c189295ff0e1b0bb9c33f7c5c4b56f46ae165
zaoqi <zaoqiff@gmail.com>
2017-04-09 17:56:01 +0800

    增加readCoding,readCode,readEnd,readCodeX,readAtom

--- src/Zaoqilc/Code.hs
+++ src/Zaoqilc/Code.hs
@@ -13,6 +13,7 @@
 
 --You should have received a copy of the GNU Affero General Public License
 --along with this program.  If not, see <http://www.gnu.org/licenses/>.
+{-# LANGUAGE MultiWayIf #-}
 module Zaoqilc.Code (
     Code(Atom, Symbol, Integer, List),
     RawCode(RawCode)
@@ -27,3 +28,20 @@ instance Show (RawCode a) where
 
 instance (Integral a) => Read (RawCode a) where
     readsPrec _ s = [(RawCode (zip [1..] s), "")]
+
+readCoding _ "" = ("", "")
+readCoding end l@(x:xs) = if end x then ("", l)
+                                   else let (a, b) = readCoding end xs
+                                        in (x:a, b)
+readCode _ _ "" = Nothing
+readCode begin end (x:xs) = if begin x
+                              then let o@(a, b) = readCoding end xs
+                                   in if a/="" then Just o
+                                               else Nothing
+                              else Nothing
+readEnd c = c `elem` [' ', '(', ')', '{', '}']
+readCodeX begin end ok s = do
+                             (a, b) <- readCode begin end s
+                             return (ok a, b)
+
+readAtom = readCode (==':') readEnd
