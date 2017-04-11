af7ebecee8e421937ae5b4d30fadd9ad72b9ddac
zaoqi <zaoqiff@gmail.com>
2017-04-11 15:35:38 +0800

    修复readSpace,增加readingList,未测试

--- src/Zaoqilc/Code.hs
+++ src/Zaoqilc/Code.hs
@@ -54,8 +54,15 @@ readAtom = readCodeX (\(_,x)->x==':') readEnd (\c->Atom (rawCodeGetPos c) (tail 
 readSimpleSymbol :: (Eq a) => [(a, Char)] -> Maybe (Code a, RawCode a)
 readSimpleSymbol = readCodeX (not . readEnd) readEnd (\c->Symbol (rawCodeGetPos c) [unreadRawCode c])
 readSpace c = do
-                (a, b) <-pure$ break (\c->not$elem$space) c
+                (a, b) <-pure$ break (\c->not$elem$space c) c
                 guard $ not $ null a
                 return b
+readingList [] = []
+readingList c = case readSpace c of
+                  Just x -> readingList x
+                  Nothing -> do
+                               (a, b) <-pure$ break (elem space) c
+                               guard $ not $ null a
+                               return $ b:readingList b
 --readingItem (x:xs) n = do
 --                         
