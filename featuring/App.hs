7773e7715eb2363696d12918d9fec7e77ec4814e
zaoqi <zaoqiff@gmail.com>
2017-05-01 15:36:03 +0800

    重写App,Call,Self

--- featuring/App.hs
+++ featuring/App.hs
@@ -1,20 +1,54 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+--Zaoqilc
+--Copyright (C) 2017  Zaoqi
+
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Affero General Public License as published
+--by the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Affero General Public License for more details.
+
+--You should have received a copy of the GNU Affero General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
+
 import Data.Dynamic
-import Data.Typeable
+
+newtype LP a = LP a
+newtype DP a = DP a
+newtype D a = D a
+
+withLP = LP
+withDP = DP
+
 class App t where
     app :: t
-instance App (([a] -> b) -> a -> ([a] -> b)) where
-    app f p = \a -> f (p:a)
-instance App ((a -> b) -> (a -> b)) where
-    app = id
-instance (Typeable b) => App (([Dynamic] -> a) -> b -> ([Dynamic] -> a)) where
-    app f p = \a -> f (toDyn p:a)
 
-class Call f r where
-    call :: f -> r
-instance Call ([a] -> b) b where
-    call f = f []
-instance Call a a where
-    call = id
+instance (a ~ c, d ~ (LP ([a] -> b))) => App ((LP ([a] -> b)) -> c -> d) where
+    app (LP f) p = LP (\a -> f (p:a))
+
+instance (a ~ c, (D b) ~ d) => App ((a -> b) -> c -> d) where
+    app f p = D $ f p
+
+instance (Typeable b, c ~ (DP ([Dynamic] -> a))) => App ((DP ([Dynamic] -> a)) -> b -> c) where
+    app (DP f) p = DP (\a -> f (toDyn p:a))
+
+instance App (f -> a -> b) => App ((D f) -> a -> b) where
+    app (D f) p = app f p
+
+class Call t where
+    call :: t
+
+instance a ~ b => Call ((D a) -> b) where
+    call (D x) = x
+
+instance b ~ c => Call ((LP ([a] -> b)) -> c) where
+    call (LP f) = f []
+
+instance b ~ c => Call ((DP ([Dynamic] -> b)) -> c) where
+    call (DP f) = f []
