77d2fd7df811bc3c62c08b6d4238da342e63bb56
zaoqi <zaomir@outlook.com>
2017-05-23 16:06:26 +0800

    重写Zaoqilc.Code

--- src/Zaoqilc/Code.hs
+++ src/Zaoqilc/Code.hs
@@ -14,54 +14,14 @@
 --You should have received a copy of the GNU Affero General Public License
 --along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE MultiWayIf #-}
-module Zaoqilc.Code (
-    Code(Atom, Symbol, Integer, List),
-    RawCode,
-    showRawCode,
-    readRawCode,
-    unreadRawCode,
-    rawCodeGetPos
-    ) where
+module Zaoqilc.Code where
 import Control.Monad
+import Data.Ratio
 
-data Code a = Atom a String | Symbol a [String] | Integer a Integer | List a [Code a] deriving (Show, Read)
-type RawCode a = [(a, Char)]
-
-showRawCode ((_,c):xs) = c:showRawCode xs
-showRawCode [] = []
-readRawCode = zip [1..]
-unreadRawCode = snd . unzip
-rawCodeGetPos = fst . head
-
-readCoding _ [] = ([], [])
-readCoding end l@(x:xs) = if end x then ([], l)
-                                   else let (a, b) = readCoding end xs
-                                        in (x:a, b)
-readCode _ _ [] = Nothing
-readCode begin end l@(x:xs) = if begin x
-                              then let o@(a, b) = readCoding end l
-                                   in if a/=[] then Just o
-                                               else Nothing
-                              else Nothing
-readEnd (_, c) = c `elem` [' ', '(', ')', '{', '}']
-space = [' ', '\t', '\n']
-readCodeX begin end ok s = do
-                             (a, b) <- readCode begin end s
-                             return (ok a, b)
-
-readAtom :: (Eq a) => RawCode a -> Maybe (Code a, RawCode a)
-readAtom = readCodeX (\(_,x)->x==':') readEnd (\c->Atom (rawCodeGetPos c) (tail $ unreadRawCode c))
-readSimpleSymbol :: (Eq a) => [(a, Char)] -> Maybe (Code a, RawCode a)
-readSimpleSymbol = readCodeX (not . readEnd) readEnd (\c->Symbol (rawCodeGetPos c) [unreadRawCode c])
-readSpace c = do
-                (a, b) <-pure$ break (\(_,x)->not$elem x space) c
-                guard $ not $ null a
-                return b
-readingList _ [] = []
-readingList f (x:xs) = if f x then 
-                              else
-readingList c = case readSpace c of
-                  Just x -> readingList x
-                  Nothing -> let (a, b) = break (\(_,x)->elem x space) c in a:readingList b
---readingItem (x:xs) n = do
---                         
+type RawCode a = (a, Char)
+type Token a = [RawCode a]
+data Code a = CodeAtom [a] [String] |
+              CodeSymbol [a] [String] |
+			  CodeNumber [a] (Ratio Integer) |
+			  CodeChar [a] Char |
+			  CodeList [a] [Code a]
