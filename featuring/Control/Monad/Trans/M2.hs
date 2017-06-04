20f8313fae5bbf73ed79c8576cbd43b90dcc95b9
zaoqi <zaomir@outlook.com>
2017-06-04 19:40:50 +0800

    更新M2,开始Vec

--- featuring/Control/Monad/Trans/M2.hs
+++ featuring/Control/Monad/Trans/M2.hs
@@ -4,7 +4,9 @@ import Control.Monad
 import Control.Monad.Trans
 import Data.Foldable
 
-newtype M2 f g a = M2 {runM2 :: f (g a)}
+newtype M2 f g a = M2 (f (g a)) deriving (Show)
+
+runM2 (M2 x) = x
 
 instance (Functor f,Functor g) => Functor (M2 f g) where
     fmap f (M2 x) = M2 $ fmap (fmap f) x
